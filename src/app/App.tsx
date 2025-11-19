75aaecb7c7022fb3c61a73d727894cf56678311b
Kirill Romanov <workrkm@mail.ru>
2025-11-19 18:46:40 +0300

    chore: init react-ts project and setup aliases

---
+++ src/app/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import '../index.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+      Conversion expirement chart
+    </div>
+    </>
+  )
+}
+
+export default App
