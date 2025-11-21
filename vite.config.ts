acc5a1c5b5b34c94e9bb78fc9b972f52d2b8f24d
Kirill Romanov <workrkm@mail.ru>
2025-11-21 20:22:56 +0300

    fix: add base Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/kameleoon-test/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     }
-  }
+  },
 })
